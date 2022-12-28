3017a648888b3f477ee8e1200d03d340368d3eeb
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2022-12-28 15:55:48 +0530

    changes to delete user

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -10,6 +10,6 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controller.GetUsers())
-	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+	incomingRoutes.DELETE("/users/:user_id", controller.GetUser())
 	//incomingRoutes.DELETE("/users/delete", controller.deleteUser())
 }
