6d2146798f3e14bf7ad932804c308f2b13c9fe71
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2023-02-06 21:07:07 +0530

    changed upload and download

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -11,7 +11,4 @@ func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controller.GetUsers())
 	incomingRoutes.DELETE("/users/:user_id", controller.GetUser())
-	//incomingRoutes.DELETE("/users/delete", controller.deleteUser())
-	incomingRoutes.POST("/upload/:image_name", controller.UploadFile())
-	incomingRoutes.GET("/download/:image_name", controller.DownloadFile())
 }
